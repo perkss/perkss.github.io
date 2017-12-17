f45ae23643b689a81d7cab33227bc8a1726e96d8
Stuart Perks <Stuart@Stuarts-MacBook-Pro.local>
2017-12-17 09:42:48 +0000

    Correct type

--- build/bundle.js
+++ build/bundle.js
@@ -31304,7 +31304,7 @@ var Home = function (_React$Component) {
         _react2.default.createElement(
           "p",
           null,
-          "Hello and congratulatins on finding this Blog. It will be a treasure chest of technical and mathematical knowledge for all those interested in computer science and programming to learn. Suitable for all but especially for those who like to learn with all the details and solid examples, no more algorithm teachers assumptions. 'What the heck is that sigma notation?' Change Build in PROGRESS!"
+          "Hello and congratulations on finding this Blog. It will be a treasure chest of technical and mathematical knowledge for all those interested in computer science and programming to learn. Suitable for all but especially for those who like to learn with all the details and solid examples, no more algorithm teachers assumptions. 'What the heck is that sigma notation?' Change Build in PROGRESS!"
         )
       );
     }
