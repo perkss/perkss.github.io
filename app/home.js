3eba25a8539fc90e123e43f6efe26a2259d0a062
Stuart Perks <Stuart@Stuarts-MacBook-Pro.local>
2017-12-19 08:21:34 +0000

    Update the name

--- app/home.js
+++ app/home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h2>The Ramblings' of EggPlant</h2>
+        <h2>The Ramblings' of Perkss</h2>
        <p>Hello and congratulations on finding this Blog. It will be a treasure chest of technical and mathematical knowledge for all those interested in computer science and programming to learn. Suitable for all but especially for those who like to learn with all the details and solid examples, no more algorithm teachers assumptions. 'What the heck is that sigma notation?'</p>
 
 <p>Build in PROGRESS!</p>
