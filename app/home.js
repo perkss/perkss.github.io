307cb8a915f549d27c11aad5fe94545afa465e02
perkss <stuart.m.perks@gmail.com>
2022-01-13 11:59:14 +0000

    Update home page

--- app/home.js
+++ app/home.js
@@ -70,12 +70,23 @@ const home = {
 
     ],
     text: [<div>
-        <p>Hello and congratulations on finding <strong>The Perks of Computer Science</strong>. This is my memory dump
+
+        <p>Hello my name is Stuart, I am a software engineer living in London UK. Please hit me up and say hello on <a
+            href={"https://www.linkedin.com/in/stuart-perks/"}>LinkedIn</a>.</p>
+
+        <p>I am currently a technical lead, software engineer with a focus on distributed systems working heavily with
+            Kotlin, Kafka and Kafka streams.</p>
+
+        <p>Please check out my <a href="https://github.com/perkss">Github</a> and feel free to contribute to any of my
+            projects. Some of the open source projects I have contributed to are Kafka, Beam, Reactor-Kafka and Samza</p>
+
+        <p>Congratulations on finding <strong>The Perks of Computer Science</strong>. This is my memory dump
             of knowledge and a place to structure and focus my learning. I will have some very
-            basic stuff such as the Maths starting very basic but then journeying into more complex topics such as
+            basic stuff such as the Maths starting very basic (got to have those foundations solid) but then journeying into more complex topics such as
             Distributed Algorithms please scan through the sections for more in depth content pages.</p>
+
         <p>Please check the side menu on the top left or the buttons at the bottom to link to the sections. Some of the
-            topics I cover are the <strong>Fundamentals of Mathematics&#39;s</strong> starting with some BiteSize Maths
+            topics I cover are the <strong>Fundamentals of Mathematics&#39;s</strong> starting with
             a few
             Prerequisites from Stanford and finally looking in depth at the book Concrete Mathematics. A study
             of <strong>Mathematics for Computer Science</strong> using course content from <a
@@ -86,15 +97,15 @@ const home = {
                 Programming</strong> using the text book <a href="https://mitpress.mit.edu/sicp/">Structure and
                 Interpretation of Computer Programs (SICP)</a> as the basis and Clojure as the main language. <strong>Hipster
                 Clojure</strong> contains some great insight into our Clojure projects as well as some blog posts on
-            interesting work going on in that space and some of the open source frameworks available such as Kafka and Cassandra! The Kafka section
+            interesting work going on in that space and some of the open source frameworks available such as Kafka and
+            Cassandra! The Kafka section
             is super interesting and is very full would definitely recommend it! Check out <strong>Low Level
                 Programming</strong> for some Pointer Madness, Memory Models and programming close to the machine. Which
             then leads to some <strong>Distributed Systems</strong> knowledge. Finally a section
             on the overused term <strong>DevOps</strong> which will cover a large range of items.</p>
         <p><strong>Most of all have fun!</strong></p>
-        <p><a href="https://github.com/perkss">Please check out my Github page and feel free to contribute to any of my
-            projects.</a></p>
-        <p><a href="../rss/clojure-rss.xml">RSS</a></p>
+
+
     </div>]
 
 };
