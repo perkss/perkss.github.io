858b94ac75bbd5567b514a8439f0e224973e72e7
Stuart <stuart.m.perks@gmail.com>
2022-02-26 21:04:59 +0000

    Update home page (#46)

--- app/home.js
+++ app/home.js
@@ -85,52 +85,85 @@ const home = {
     text: [
         <div>
 
-            <img style={{float: "left", marginRight: 25, marginBottom: 15 }} src={me} alt="Me" width="20%" height="20%"/>
+            <br/>
+            <img style={{float: "left", marginRight: 25, marginBottom: 15}} src={me} alt="Me" width="20%" height="20%"/>
 
-            <p className={"cv"}>Hello my name is <i>Stuart Perks</i>. I am a software engineer living in London UK. I have a keen
-            interest in Distributed
-            Systems with a focus on database internals and stream processing. Some of the open source projects I have
-            contributed to are Kafka, Beam, Reactor-Kafka and Samza.</p>
-        <p className={"cv"}>Please hit me up and say hello on LinkedIn <SocialIcon style={{ height: 35, width: 35 }} url="https://www.linkedin.com/in/stuart-perks/"/> and
-            check out my Github <SocialIcon style={{ height: 35, width: 35 }} url="https://github.com/perkss"/>.</p>
+            <p className={"cv"}>Hello my name is <i>Stuart Perks</i>. I am an experienced software engineer living in
+                London UK. I
+                have a keen
+                interest in Distributed
+                Systems with a focus on database internals and stream processing. Some of the open source projects I
+                have
+                contributed to are Kafka, Beam, Reactor-Kafka and Samza.</p>
+            <p className={"cv"}>Please hit me up and say hello on LinkedIn <SocialIcon style={{height: 35, width: 35}}
+                                                                                       url="https://www.linkedin.com/in/stuart-perks/"/> and
+                check out my Github <SocialIcon style={{height: 35, width: 35}} url="https://github.com/perkss"/>.</p>
 
-        <p className={"cv"}>I am currently a technical lead, software engineer with a focus on distributed systems working heavily with
-            Kotlin, Kafka and Kafka streams.</p>
+            <p className={"cv"}>I am currently a technical lead, software engineer with a focus on distributed systems
+                working heavily with
+                Kotlin, Kafka and Kafka streams.</p>
 
-        <p className={"cv"}>Please check out my <a href="https://github.com/perkss">Github</a> and feel free to contribute to any of my
-            projects. </p>
+            <p className={"cv"}>Please check out my <a href="https://github.com/perkss">Github</a> and feel free to
+                contribute to any of my
+                projects. </p>
 
-        {/*<ColoredLine color="#0288D1"/>*/}
-        <br/>
+            {/*<ColoredLine color="#0288D1"/>*/}
+            <br/>
 
-        <p>Congratulations on finding <strong>The Perks of Computer Science</strong>. This is my memory dump
-            of knowledge and a place to structure and focus my learning. I will have some very
-            basic stuff such as the Maths starting very basic (got to have those foundations solid) but then journeying
-            into more complex topics such as
-            Distributed Algorithms please scan through the sections for more in depth content pages.</p>
+            <h3>Publications</h3>
 
-        <p>Please check the side menu on the top left or the buttons at the bottom to link to the sections. Some of the
-            topics I cover are the <strong>Fundamentals of Mathematics&#39;s</strong> starting with
-            a few
-            Prerequisites from Stanford and finally looking in depth at the book Concrete Mathematics. A study
-            of <strong>Mathematics for Computer Science</strong> using course content from <a
-                href="https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-042j-mathematics-for-computer-science-spring-2015/index.htm">MIT</a> and <a
-                href="http://web.stanford.edu/class/cs103/">Stanford</a> follows the fundamental
-            knowledge. <strong>Algorithms</strong> will then be discussed in detail building them using a variety of
-            languages such as C, Kotlin, Java, Python and Clojure. We then have a dig into the world of <strong>Functional
-                Programming</strong> using the text book <a href="https://mitpress.mit.edu/sicp/">Structure and
-                Interpretation of Computer Programs (SICP)</a> as the basis and Clojure as the main language. <strong>Hipster
-                Clojure</strong> contains some great insight into our Clojure projects as well as some blog posts on
-            interesting work going on in that space and some of the open source frameworks available such as Kafka and
-            Cassandra! The Kafka section
-            is super interesting and is very full would definitely recommend it! Check out <strong>Low Level
-                Programming</strong> for some Pointer Madness, Memory Models and programming close to the machine. Which
-            then leads to some <strong>Distributed Systems</strong> knowledge. Finally a section
-            on the overused term <strong>DevOps</strong> which will cover a large range of items.</p>
-        <p><strong>Most of all have fun!</strong></p>
+            <ul className="no-bullets" style={{paddingLeft: 10}}>
+                <li style={{margin: 10}}>
+                    <i><a href={"https://ieeexplore.ieee.org/abstract/document/7515618"}>Tolerating transient
+                        late-timing
+                        faults in cloud-based real-time stream processing.</a> In Real-Time
+                        Distributed Computing (ISORC), 2016 IEEE 19th International Symposium on, pp. 108-115. IEEE,
+                        2016.</i>
+                </li>
+
+                <li style={{margin: 10}}>
+                    <i>
+                        <a href={"https://www.researchgate.net/profile/Stuart-Perks/publication/286580029_Energy_Efficient_Software_Engineering_for_Cloud_Computing/links/566be88908ae62b05f07c554/Energy-Efficient-Software-Engineering-for-Cloud-Computing.pdf"}>Energy
+                            Efficient Software Engineering for Cloud Computing</a>
+                    </i>
+                </li>
+            </ul>
+
+            <h3>Perks of Computer Science</h3>
+
+            <p>Congratulations on finding <strong>The Perks of Computer Science</strong>. This is my memory dump
+                of knowledge and a place to structure and focus my learning. I will have some very
+                basic stuff such as the Maths starting very basic (got to have those foundations solid) but then
+                journeying
+                into more complex topics such as
+                Distributed Algorithms please scan through the sections for more in depth content pages.</p>
+
+            <p>Please check the side menu on the top left or the buttons at the bottom to link to the sections. Some of
+                the
+                topics I cover are the <strong>Fundamentals of Mathematics&#39;s</strong> starting with
+                a few
+                Prerequisites from Stanford and finally looking in depth at the book Concrete Mathematics. A study
+                of <strong>Mathematics for Computer Science</strong> using course content from <a
+                    href="https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-042j-mathematics-for-computer-science-spring-2015/index.htm">MIT</a> and <a
+                    href="http://web.stanford.edu/class/cs103/">Stanford</a> follows the fundamental
+                knowledge. <strong>Algorithms</strong> will then be discussed in detail building them using a variety of
+                languages such as C, Kotlin, Java, Python and Clojure. We then have a dig into the world of <strong>Functional
+                    Programming</strong> using the text book <a href="https://mitpress.mit.edu/sicp/">Structure and
+                    Interpretation of Computer Programs (SICP)</a> as the basis and Clojure as the main
+                language. <strong>Hipster
+                    Clojure</strong> contains some great insight into our Clojure projects as well as some blog posts on
+                interesting work going on in that space and some of the open source frameworks available such as Kafka
+                and
+                Cassandra! The Kafka section
+                is super interesting and is very full would definitely recommend it! Check out <strong>Low Level
+                    Programming</strong> for some Pointer Madness, Memory Models and programming close to the machine.
+                Which
+                then leads to some <strong>Distributed Systems</strong> knowledge. Finally a section
+                on the overused term <strong>DevOps</strong> which will cover a large range of items.</p>
+            <p><strong>Most of all have fun!</strong></p>
 
 
-    </div>]
+        </div>]
 
 };
 
